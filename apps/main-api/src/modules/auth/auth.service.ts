89892e00dc032ae1181b5c119f818577d4dccfac
Bùi Đỗ Duy Quân <21127141@student.hcmus.edu.vn>
2024-07-25 14:16:50 +0700

    feat(vou-23): implement jwt guard

--- apps/main-api/src/modules/auth/auth.service.ts
+++ apps/main-api/src/modules/auth/auth.service.ts
@@ -1,12 +1,13 @@
 import { HttpException, HttpStatus, Inject, Injectable } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
 import { ClientOptions, ClientProxy, ClientProxyFactory } from "@nestjs/microservices";
-import { CreateAccountDto } from "@types";
-import { catchError } from "rxjs";
+import { CreateAccountDto, ILoginResponse } from "@types";
+import { catchError, lastValueFrom } from "rxjs";
 
 @Injectable()
 export class AuthService {
   private _client: ClientProxy;
-  constructor(@Inject("USER_SERVICE") options: ClientOptions) {
+  constructor(@Inject("USER_SERVICE") options: ClientOptions, private readonly jwtService: JwtService) {
     this._client = ClientProxyFactory.create(options);
   }
 
@@ -23,8 +24,9 @@ export class AuthService {
   }
 
   async login(phone: string, password: string) {
+    const loginResponse: ILoginResponse = { access_token: "" };
     const pattern = { method: "POST", path: "/account/verify-account" };
-    return this._client.send(pattern, { phone, password }).pipe(
+    const res = await this._client.send(pattern, { phone, password }).pipe(
       catchError((error) => {
         const statusCode = error.status || HttpStatus.INTERNAL_SERVER_ERROR;
         const message = error.message || "An error occurred";
@@ -32,5 +34,12 @@ export class AuthService {
         throw new HttpException(message, statusCode);
       })
     );
+
+    const data = await lastValueFrom(res);
+    loginResponse.access_token = this.jwtService.sign({
+      userId: data.id,
+      role: data.role,
+    });
+    return loginResponse;
   }
 }
