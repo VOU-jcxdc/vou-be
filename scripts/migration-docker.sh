3fbfdf1f363dfb0c0d7b3a0e2b228a3e0f29deac
ducviet5138 <ducviet5138@gmail.com>
2024-07-28 14:05:24 +0700

    fix(vou-19): update migration script

--- scripts/migration-docker.sh
+++ scripts/migration-docker.sh
@@ -1,13 +1,30 @@
-#!/bin/bash
-echo "Starting migration"
-
-# Find all typeorm.config.ts files matching the pattern and store them in an array
-configs=($(find . -type f -name "typeorm-docker.config.ts" -path "*/apps/*/*"))
-
-# Iterate over the array and run the migration command for each config
-for config in "${configs[@]}"; do
-    npx typeorm-ts-node-commonjs migration:run -d "$config"
-done
-
-# Notfiy the user that the migration is complete
+#!/bin/bash 
+echo "Starting migration" 
+ 
+# Rename the .env.build file to .env in *-service directories, overriding any existing .env file 
+build_envs=($(find . -type f -name ".env.build" -path "*/apps/*-service/*")) 
+ 
+# Echo all paths to the .env.build files 
+for build_env in "${build_envs[@]}"; do 
+    echo "Renaming $build_env to ${build_env/.build/}" 
+    mv -f "$build_env" "${build_env/.build/}" 
+done 
+ 
+# Find all typeorm.config.ts files matching the pattern and store them in an array 
+configs=($(find . -type f -name "typeorm.config.ts" -path "*/apps/*-service/*")) 
+ 
+# Iterate over the array and run the migration command for each config 
+migration_validation=true 
+for config in "${configs[@]}"; do 
+    # Check if the migration is valid 
+    if ! npx typeorm-ts-node-commonjs migration:run -d "$config"; then 
+        migration_validation=false 
+    fi 
+done 
+ 
+# Notfiy the user that the migration is complete 
+if [ "$migration_validation" = false ]; then 
+    echo "Migration failed" 
+    exit 1 
+fi 
 echo "Migration complete"
