90271a7bf62d1e9439d3578f84aa537d3a7d6947
Bùi Đỗ Duy Quân <21127141@student.hcmus.edu.vn>
2024-07-28 18:17:42 +0700

    ci(vou-19): config microservice connection in docker compose

--- scripts/migration-docker.sh
+++ scripts/migration-docker.sh
@@ -1,15 +1,6 @@
 #!/bin/bash 
 echo "Starting migration" 
  
-# Rename the .env.build file to .env in *-service directories, overriding any existing .env file 
-build_envs=($(find . -type f -name ".env.build" -path "*/apps/*-service/*")) 
- 
-# Echo all paths to the .env.build files 
-for build_env in "${build_envs[@]}"; do 
-    echo "Renaming $build_env to ${build_env/.build/}" 
-    mv -f "$build_env" "${build_env/.build/}" 
-done 
- 
 # Find all typeorm.config.ts files matching the pattern and store them in an array 
 configs=($(find . -type f -name "typeorm.config.ts" -path "*/apps/*-service/*")) 
  
