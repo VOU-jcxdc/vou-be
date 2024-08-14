59e053b9c0dfe27a372bdf3c284853b772e3c4a9
ducviet5138 <ducviet5138@gmail.com>
2024-08-14 22:38:56 +0700

    feat(vou-51): add default value for game_images

--- apps/event-service/src/migrations/1723639108137-add_table_games_default_data.ts
+++ apps/event-service/src/migrations/1723639108137-add_table_games_default_data.ts
@@ -6,14 +6,14 @@ export class AddTableGamesDefaultData1723639108137 implements MigrationInterface
       INSERT INTO games (id, name, type, exchange_status, instruction)
       VALUES 
         (
-          uuid_generate_v4(), 
+          'd463bd0b-99ae-4070-b4e4-5d390ec3211c', 
           'Real-time Quiz Game', 
           'realtime_quiz_game', 
           false, 
           'Objective: Answer as many questions correctly within the given time limit.\\nGameplay:\\n1. Players join the game and wait for the quiz to start.\\n2. Questions are displayed one at a time, and players must select the correct answer from multiple choices.\\n3. Points are awarded for each correct answer, and the faster the response, the more points are earned.\\n4. The game continues until the time limit is reached or all questions are answered.\\n5. The player with the highest score at the end of the game wins.\\nRules:\\n1. No cheating or using external help.\\n2. Players must answer within the time limit for each question.'
         ),
         (
-          uuid_generate_v4(), 
+          '8340911c-a33e-4d59-9669-093fb2db46cd', 
           'Shake Game', 
           'shake_game', 
           true, 
