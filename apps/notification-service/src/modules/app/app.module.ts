d1e08715c7c1feb1a519ec9528103cb455cf001a
Bùi Đỗ Duy Quân <21127141@student.hcmus.edu.vn>
2024-08-26 10:39:23 +0700

    fix: remove duplicate import library

--- apps/notification-service/src/modules/app/app.module.ts
+++ apps/notification-service/src/modules/app/app.module.ts
@@ -1,10 +1,6 @@
 import { Module } from "@nestjs/common";
 import { MongoDatabaseModule } from "@database";
 import { NotificationModelModule } from "../model/notification-model.module";
-import { NotificationModule } from "../notification/notification.module";
-import { ConfigModule } from "@nestjs/config";
-import { FirebaseModule } from "@shared-modules";
-import { ConfigModule } from "@nestjs/config";
 
 @Module({
   imports: [MongoDatabaseModule, NotificationModelModule],
